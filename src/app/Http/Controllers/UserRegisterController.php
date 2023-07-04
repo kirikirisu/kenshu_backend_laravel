00446713eeab69809866b238791e97167b263653
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-04 16:22:07 +0900

    Add user resister controller

---
+++ src/app/Http/Controllers/UserRegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
+use App\Interfaces\UserRepositoryInterface;
+
+class UserRegisterController extends Controller
+{
+    private UserRepositoryInterface $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository) {
+        $this->userRepository = $userRepository;
+    }
+
+    public function index(Request $request) {
+        $user = $request->only(['name', 'email', 'password']);
+
+        $insertedUser = $this->userRepository->insertUser($user);
+
+        return response()->json([
+            'data' => $insertedUser
+        ],
+            Response::HTTP_CREATED
+        );
+    }
+}
