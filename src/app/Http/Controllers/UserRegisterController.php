3b31c58ae6589aae6ec1b0bde27e54bdbca90009
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-04 16:53:50 +0900

    Add strict anotation

--- src/app/Http/Controllers/UserRegisterController.php
+++ src/app/Http/Controllers/UserRegisterController.php
@@ -1,11 +1,10 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
-use App\Repositories\UserRepository;
 use App\Interfaces\UserRepositoryInterface;
 
 class UserRegisterController extends Controller
