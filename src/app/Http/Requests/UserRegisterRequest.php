e5a127301b3a83478662aa1ff802167329b0c32e
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-04 19:16:30 +0900

    Add user register validation

---
+++ src/app/Http/Requests/UserRegisterRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:100|string',
+            'email' => 'required|email|max:100|string',
+            'password' => 'required|max:100|string',
+            'avatar' => 'nullable|file|mimes:jpeg,png|max:2048'
+        ];
+    }
+}
