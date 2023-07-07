a3c16fc86c65466ddc53ee7429a9fdf631dc993c
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-07 23:10:32 +0900

    Add user login test

---
+++ src/app/Http/Requests/LoginUserRequest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'min:1', 'max:100',  'string'],
+            'password' => ['required', 'min:4',  'max:100', 'string'],
+        ];
+    }
+}
