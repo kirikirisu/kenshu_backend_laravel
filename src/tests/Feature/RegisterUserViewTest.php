08a551bf653351fce536b4342692a3100d276dd9
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-10 17:22:02 +0900

    Add test

---
+++ src/tests/Feature/RegisterUserViewTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class RegisterUserViewTest extends TestCase
+{
+    public function test_ユーザー登録画面にアクセスすると200が帰ってくること(): void
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+}
