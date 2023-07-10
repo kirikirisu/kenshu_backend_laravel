b29f35fb8c4bc4228ab99379329c0883898e92ef
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-10 12:52:51 +0900

    Fix User model for auth

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -3,16 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+
+
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $icon_url
+ * @method bool save(array $options)
+ */
+class User extends Model implements 
+    AuthenticatableContract,
+    AuthorizableContract
 {
-    use HasUuids;
+    use HasUuids, Authenticatable, Authorizable;
 
     protected $fillable = ['id', 'name', 'email', 'icon_url'];
 
-    protected $hidden = ['password'];
+    protected $hidden = ['password', 'remember_token'];
 
     protected $casts = ['password' => 'hashed'];
 
