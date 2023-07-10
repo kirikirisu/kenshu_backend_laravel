66c6a1ca156745e0776c959fa857dfed6223c64f
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-10 13:14:13 +0900

    Fix test

--- src/tests/Feature/RegisterUserControllerTest.php
+++ src/tests/Feature/RegisterUserControllerTest.php
@@ -27,7 +27,7 @@ class RegisterUserControllerTest extends TestCase
         $this->assertTrue(Auth::check());
     }
 
-    public function test_名前入力に不足があった場合にエラーと共に登録画面にリダイレクトすること()
+    public function test_入力に不足があった場合にエラーと共に登録画面にリダイレクトすること()
     {
         $data = [
             'name' => '',
@@ -36,53 +36,9 @@ class RegisterUserControllerTest extends TestCase
         ];
 
         $response = $this->post('/register', $data);
+
         $response->assertStatus(302);
         $response->assertRedirect('/register');
         $response->assertSessionHasErrors(['name']);
-    }
-
-    public function test_メールアドレスに不足があった場合にエラーと共に登録画面にリダイレクトすること()
-    {
-        $data = [
-            'name' => 'jhon',
-            'email' => '',
-            'password' => 'password123',
-        ];
-
-        $response = $this->post('/register', $data);
-        $response->assertStatus(302);
-        $response->assertRedirect('/register');
-        $response->assertSessionHasErrors(['email']);
-    }
-
-    public function test_パスワードに不足があった場合にエラーと共に登録画面にリダイレクトすること()
-    {
-        $data = [
-            'name' => 'jhon',
-            'email' => 'jhon@example.com',
-            'password' => '',
-        ];
-
-        $response = $this->post('/register', $data);
-        $response->assertStatus(302);
-        $response->assertRedirect('/register');
-        $response->assertSessionHasErrors(['password']);
-    }
-
-    public function test_ファイルに不足があった場合にエラーと共に登録画面にリダイレクトすること()
-    {
-        $svg_file = UploadedFile::fake()->image('awsome.svg');
-
-        $data = [
-            'name' => 'jhon',
-            'email' => 'jhon@example.com',
-            'password' => 'password',
-            'avatar' => $svg_file
-        ];
-
-        $response = $this->post('/register', $data);
-        $response->assertStatus(302);
-        $response->assertRedirect('/register');
-        $response->assertSessionHasErrors(['avatar']);
     }
 }
