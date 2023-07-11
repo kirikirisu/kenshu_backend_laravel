53a1f125f024a91a47ca8136a87753ac5a0c3246
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-11 19:11:37 +0900

    Rename posts thumbnail_id

---
+++ src/database/migrations/2023_07_11_100618_rename_thumbnail_id_column_in_posts.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->renameColumn('thumbnail_id', 'thumbnail');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->renameColumn('thumbnail', 'thumbnail_id');
+        });
+    }
+};
