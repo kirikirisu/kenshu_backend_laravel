5b8e60c87db5b287b5c5609519d9a39c6a9f4997
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-12 19:29:55 +0900

    Add property

--- src/app/Models/Image.php
+++ src/app/Models/Image.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $post_id
+ * @property string $url
+ */
 class Image extends Model
 {
     use HasUuids;
