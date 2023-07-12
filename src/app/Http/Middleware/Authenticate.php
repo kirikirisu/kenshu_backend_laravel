683f9670c88b02be2e029b115c79b27f7042aae8
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-12 12:18:41 +0900

    Fix auth middleware return type

--- src/app/Http/Middleware/Authenticate.php
+++ src/app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
@@ -10,7 +11,7 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo(Request $request): RedirectResponse
     {
         return $request->expectsJson() ? null : response()->redirectTo('login');
     }
