4d8889e25883161dd2940bcdb5ce3eb7f1315da6
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-13 10:07:24 +0900

    Implement PostUploadedFile

--- src/app/Infrastructure/Disk/PostUploadedFileRepository.php
+++ src/app/Infrastructure/Disk/PostUploadedFileRepository.php
@@ -4,20 +4,38 @@ namespace App\Infrastructure\Disk;
 
 use App\Domain\Repository\PostUploadedFileRepositoryInterface;
 use Illuminate\Http\UploadedFile;
+use App\Domain\Result\FileUploadResult;
 
 class PostUploadedFileRepository implements PostUploadedFileRepositoryInterface
 {
-  public function save(UploadedFile $thumbnail): string 
+  /**
+   * @param UploadedFile $thumbnail
+   * @return FileUploadResult
+   */
+  public function save(UploadedFile $thumbnail): FileUploadResult
   {
-    return 'a';
+    $file_name = $thumbnail->getClientOriginalName();
+    $saved_file_path = $thumbnail->storeAs('', $file_name);
+
+    if (!$saved_file_path) $save_result_list[] = new FileUploadResult(file_name: $file_name, saved_file_path: null, status: false);
+    return new FileUploadResult(file_name: $file_name, saved_file_path: $saved_file_path, status: true);
   }
 
   /**
    * @param UploadedFile[] $image_list
-   * @return string[]
+   * @return FileUploadResult[]
    */
   public function saveList(array $image_list): array
   {
-    return ['a', 'b'];
+    $save_result_list = [];
+    foreach($image_list as $image){
+      $file_name = $image->getClientOriginalName();
+      $saved_file_path = $image->storeAs('', $file_name);
+
+      if (!$saved_file_path) $save_result_list[] = new FileUploadResult(file_name: $file_name, saved_file_path: null, status: false);
+      $save_result_list[] = new FileUploadResult(file_name: $file_name, saved_file_path: $saved_file_path, status: true);
+    }
+
+    return $save_result_list;
   }
 }
