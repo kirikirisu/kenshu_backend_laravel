2c4551250092b6518b27154d368012ea6be228dd
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-19 17:28:40 +0900

    chore: add strict anotation

--- src/database/factories/PostFactory.php
+++ src/database/factories/PostFactory.php
@@ -1,9 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Database\Factories;
 
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
