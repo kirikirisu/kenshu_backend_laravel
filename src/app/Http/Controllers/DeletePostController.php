f75f57f501bf1f27242c157ca78b5f44c37bdafd
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-19 18:40:27 +0900

    Add delete

--- src/app/Http/Controllers/DeletePostController.php
+++ src/app/Http/Controllers/DeletePostController.php
@@ -3,12 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Image;
+use App\Models\PostTag;
+use Exception;
 
 class DeletePostController extends Controller
 {
     public function __invoke(Request $request): RedirectResponse
     {
-        return response()->redirectTo("/")->with('failed_delete_post', '投稿の削除に失敗しました。');
+        /** @var string $post_id */
+        $post_id = $request->route('id');
+
+        try {
+            DB::beginTransaction();
+
+            $post = Post::query()->find($post_id);
+            if (is_null($post)) return response()->redirectTo("/")->with('failed_delete_post', '投稿が見つかりませんでした。');
+
+            Image::query()->where('post_id', $post->id)->delete();
+            PostTag::query()->where('post_id', $post_id)->delete();
+
+            $post->delete();
+
+            DB::commit();
+
+            return response()->redirectTo("/");
+        } catch(Exception $e) {
+            DB::rollBack();
+            return response()->redirectTo("/")->with('failed_delete_post', '投稿の削除に失敗しました。');
+        }
     }
 }
