d45af35f6fcb000e530031ef3305c844500ccc25
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-24 11:55:59 +0900

    Fix   n+1

--- src/app/Http/Controllers/GetTopPageController.php
+++ src/app/Http/Controllers/GetTopPageController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\View\View;
 use App\Models\Tag;
 use App\Models\Post;
+use App\Models\User;
 
 class GetTopPageController extends Controller
 {
     public function __invoke(): View
     {
         $tags = Tag::all();
-        $posts = Post::query()->paginate(10);
+        $posts = Post::query()->with('tags')->with('user')->paginate(10);
 
         return view('top', ['posts' => $posts, 'tags' => $tags]);
     }
