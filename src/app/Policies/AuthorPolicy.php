6c6ce86e2c3e88d9765c57e015f4b8f6454bd25f
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-24 12:29:26 +0900

    Fix lint error

--- src/app/Policies/AuthorPolicy.php
+++ src/app/Policies/AuthorPolicy.php
@@ -4,7 +4,6 @@ namespace App\Policies;
 
 use App\Models\User;
 use App\Models\Post;
-use Illuminate\Auth\Access\Response;
 
 class AuthorPolicy
 {
@@ -12,18 +11,23 @@ class AuthorPolicy
     {
     }
 
+    /**
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function view(User $user, Post $post): bool
     {
-        return $user->id === $post->user->id;
+        return $post->user && $user->id === $post->user->id;
     }
 
     public function update(User $user, Post $post): bool
     {
-        return $user->id === $post->user->id;
+        return $post->user && $user->id === $post->user->id;
     }
     
     public function delete(User $user, Post $post): bool
     {
-        return $user->id === $post->user->id;
+        return $post->user && $user->id === $post->user->id;
     }
 }
